ba492fbfa679bd4e242554f76557bab5aaa130f4
Maximilian <scientificresearch309@gmail.com>
2024-02-21 00:30:56 +0100

    some more comments about the node environments variables

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = require('./app');
 // show us which environment we are using currently: => it shows us development
 // console.log(app.get('env'));
 console.log(process.env); // shows us a bunch of node variables
-
+// type this command in VSCode Terminal: NODE_ENV=development X=23 nodemon server.js
 // 4) START SERVER => now, our exntry point to start our application is server.js and no longer
 // app.js => i will change it in package.json for nodemon too.
 
