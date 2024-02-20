d68c533fdc5a60b08f6c1bf9e9187f61513a1c8b
Maximilian <scientificresearch309@gmail.com>
2024-02-21 00:26:16 +0100

    console.log(process.env); shows us a bunch of node variables

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 // const app = express();
 const app = require('./app');
 // show us which environment we are using currently: => it shows us development
-console.log(app.get('env'));
+// console.log(app.get('env'));
+console.log(process.env); // shows us a bunch of node variables
 
 // 4) START SERVER => now, our exntry point to start our application is server.js and no longer
 // app.js => i will change it in package.json for nodemon too.
