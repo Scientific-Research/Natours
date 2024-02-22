745da4ada2e03fed3d250956869d05df69e5ff9a
Maximilian <scientificresearch309@gmail.com>
2024-02-22 19:28:48 +0100

    I moved the mongoose schema and model from server.js to tourModel.js

--- server.js
+++ server.js
@@ -33,23 +33,7 @@ const connect = async () => {
 };
 connect();
 
-const tourSchema = new mongoose.Schema({
-   name: {
-      type: String,
-      required: [true, 'A tour must have a name'],
-      unique: true,
-   },
-   rating: {
-      type: Number,
-      default: 4.5,
-   },
-   price: {
-      type: Number,
-      required: [true, 'A tour must have a price'],
-   },
-});
 
-const Tour = mongoose.model('Tour', tourSchema);
 
 /* Tour here as Model in Mongoose is like a class in JS and we define an instance(object) of it
  => testTour*/
