06a2c705bcde05789cb1e21c69295b9a76554656
Maximilian <scientificresearch309@gmail.com>
2024-02-23 22:37:01 +0100

    running import-dev-data file directly

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+// const express = require('express');
+// dotenv.config({ path: './config.env' });
+dotenv.config({ path: './.env' });
+
+// const app = express();
+// const app = require('./app');
+
+// show us which environment we are using currently: => it shows us development
+// console.log(app.get('env'));
+// console.log(process.env); // shows us a bunch of node variables
+// type this command in VSCode Terminal: NODE_ENV=development X=23 nodemon server.js
+// 4) START SERVER => now, our exntry point to start our application is server.js and no longer
+// app.js => i will change it in package.json for nodemon too.
+
+const connect = async () => {
+   try {
+      const con = await mongoose.connect(process.env.DATABASE); // connect to MongoDB
+      // const con = await mongoose.connect(process.env.DATABASE_LOCAL); // connect locally to monogosh
+      // NOTE: we have to replace localhost with 127.0.0.1, otherwise, it will not work!
+      // NOTE: In case of using the local database, we have to run mongod in Shell Terminal!
+      // console.log(con);
+
+      console.log('MongoDB connection successful!');
+      //   const PORT = process.env.PORT || 8000;
+      //   app.listen(PORT, () => {
+      //      console.log(`Server is listening on PORT ${PORT}`);
+      //   });
+   } catch (err) {
+      console.log('Error connecting to MongoDB:', err.message);
+   }
+};
+connect();
+
+// READ JSON FILE:
+// using JSON.parse() to convert the json to javascrip objects
+// const tours = JSON.parse(fs.readFileSync('./data/tours-simple.json', 'utf-8'));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+// IMPORT DATA INTO MONGODB:
+const importData = async () => {
+   try {
+      const importedData = await Tour.create(tours);
+      console.log('Data loaded successfully from JSON data to MongoDB!');
+      res.status(201).json({
+         status: 'success',
+         importedData: importedData,
+      });
+   } catch (err) {
+      console.log(err.message);
+      res.status(400).json({ status: 'fail', message: err.message });
+   }
+};
+// importData();
+
+// DELETE ALL DATA FROM DB:
+const deleteAllDataFromDB = async () => {
+   try {
+      await Tour.deleteMany();
+      res.status(200).json({
+         status: 'success',
+      });
+   } catch (err) {
+      console.log('Error Deleting All Data From MongoDB ' + err.message);
+      res.status(400).json({ status: 'fail', message: err.message });
+   }
+};
+// deleteAllDataFromDB();
+console.log(process.argv);
