95470310ba7cf666ff76ec521f9a0d4b6813540f
Maximilian <scientificresearch309@gmail.com>
2024-02-26 13:33:44 +0100

    we published our document as duration week in form of JSON and Object and we will see it now in the Postman but not in the DB

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,59 +1,67 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-   name: {
-      type: String,
-      required: [true, 'A tour must have a name'],
-      unique: true,
-      trim: true,
+const tourSchema = new mongoose.Schema(
+   {
+      name: {
+         type: String,
+         required: [true, 'A tour must have a name'],
+         unique: true,
+         trim: true,
+      },
+      duration: {
+         type: Number,
+         required: [true, 'A tour must have a duration'],
+      },
+      maxGroupSize: {
+         type: Number,
+         required: [true, 'A tour must have a group size'],
+      },
+      difficulty: {
+         type: String,
+         required: [true, 'A tour must have a difficulty'],
+      },
+      ratingsAverage: {
+         type: Number,
+         default: 4.5,
+      },
+      ratingsQuantity: {
+         type: Number,
+         default: 0,
+      },
+      price: {
+         type: Number,
+         required: [true, 'A tour must have a price'],
+      },
+      priceDiscount: Number,
+      summary: {
+         type: String,
+         trim: true,
+         required: [true, 'A tour must have a description'],
+      },
+      description: {
+         type: String,
+         trim: true,
+      },
+      imageCover: {
+         type: String,
+         required: [true, 'A tour must have a cover image'],
+      },
+      images: [String],
+      createdAt: {
+         type: Date,
+         default: Date.now(),
+         select: false, // it will not display this item in a tour in Postman, when we get the list
+         // of the tours!
+      },
+      startDates: [Date],
    },
-   duration: {
-      type: Number,
-      required: [true, 'A tour must have a duration'],
-   },
-   maxGroupSize: {
-      type: Number,
-      required: [true, 'A tour must have a group size'],
-   },
-   difficulty: {
-      type: String,
-      required: [true, 'A tour must have a difficulty'],
-   },
-   ratingsAverage: {
-      type: Number,
-      default: 4.5,
-   },
-   ratingsQuantity: {
-      type: Number,
-      default: 0,
-   },
-   price: {
-      type: Number,
-      required: [true, 'A tour must have a price'],
-   },
-   priceDiscount: Number,
-   summary: {
-      type: String,
-      trim: true,
-      required: [true, 'A tour must have a description'],
-   },
-   description: {
-      type: String,
-      trim: true,
-   },
-   imageCover: {
-      type: String,
-      required: [true, 'A tour must have a cover image'],
-   },
-   images: [String],
-   createdAt: {
-      type: Date,
-      default: Date.now(),
-      select: false, // it will not display this item in a tour in Postman, when we get the list
-      // of the tours!
-   },
-   startDates: [Date],
-});
+   {
+      // NOTE: we have to say that we will need the Virtuals, when the data goes to be published at
+      // output!
+      toJSON: { virtuals: true }, // when the data outputted as JSON
+      toObject: { virtuals: true }, // when the data outputted as Object
+   }
+);
 
 // NOTE: durationWeeks will not persist in DB, it will be there only when get the data and then
 // will be gone!
