1fc2e440ff6a3882c305823f6d8ceef72613bf35
Maximilian <scientificresearch309@gmail.com>
2024-02-27 15:47:16 +0100

    in first step, we write our general error handling middleware and in the next step, we got to make an error to test it, whether, it works or not

--- app.js
+++ app.js
@@ -61,5 +61,28 @@ app.all('*', (req, res, next) => {
    });
 });
 
+// NOTE: defining a global Error Handler for entire project
+// this middleware function is a first Error function => it means err comes as first Parameter
+// and then comes req, res and at the end comes next
+// when we have four parameters and err comes at the beginning, Express understand automatically
+// that this is an error handling middleware!
+app.use((err, req, res, next) => {
+   // we have different error status Codes like 400,404,500,..., that's why we have to get the
+   // error from every occurred erorr and when there is no error code for the occurred error,
+   // we consider it as Error with Code 500!
+   err.statusCode = err.statusCode || 500; // if it is defined, StatusCoe is what error itself has
+   // when it is not defined, we consider it as 500 which means Internal Server Error!
+   err.status = err.status || 'error'; // it status is defined, status takes what the error has,
+   // it status is not defined, it would be 'error' which is for 500 statusCode!
+   // and when it is 400 or 404 status Code, it would be 'fail' as we write it as status in our json!
+
+   // NOTE: first of all, we see what is statusCode and after that, it gives us the related
+   // status and message for that error!
+   res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+   });
+});
+
 // Export the app from here to the Server.js
 module.exports = app;
