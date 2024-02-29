d8e16b2b570ca302642a2f915c74e89ef6fa3620
Maximilian <scientificresearch309@gmail.com>
2024-02-29 20:46:20 +0100

    we created a new User with name, email, password and passwordConfirm and also installed jasonwebtoken

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
@@ -5,7 +6,21 @@ exports.signup = catchAsync(async (req, res, next) => {
    // NOTE: when we use catchAsync(), we don't need to use try() catch() anymore! that's why
    // i comment them out there!
    //    try {
-   const newUser = await User.create(req.body);
+   // const newUser = await User.create(req.body);
+   // NOTE: using deconstructuring:
+   const { name, email, password, passwordConfirm } = req.body;
+   const newUser = await User.create({
+      name,
+      email,
+      password,
+      passwordConfirm,
+      /*
+      name: req.body.name,
+      email: req.body.name,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
+      */
+   });
 
    res.status(201).json({
       // 201 is used for creating the user!
