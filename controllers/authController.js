794773a87979cfd77432ca13685abf04d9144762
Maximilian <scientificresearch309@gmail.com>
2024-02-29 22:22:34 +0100

    we have created the login middleware in authController.js and the next step is to create the corresponding route()

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
    // NOTE: when we use catchAsync(), we don't need to use try() catch() anymore! that's why
@@ -49,5 +50,29 @@ exports.signup = catchAsync(async (req, res, next) => {
 
 // NOTE: the next step to create the signup route in which this signup function can be called!
 
+exports.login = (req, res, next) => {
+   // const email = req.body.email;
+   // const password = req.body.password;
+   // NOTE: these are the credentials that a user send to the check...
+   const { email, password } = req.body;
+
+   // 1) Check if email and password exist:
+   if (!email || !password) {
+      // we use next() to declare a new error as a parameter inside it and then send it to AppError
+      // function
+      const message = 'Please provide email and password!';
+      next(new AppError(message, 400));
+   }
+
+   // 2) Check if user exists && password is correct:
+
+   // 3) If everything is ok, send the token to the client:
+
+   const token = '';
+   res.status(200).json({
+      status: 'success',
+      token,
+   });
+};
 // NOTE: this module.exports = signup doesn't work here, we have use exports.signup = ...
 // module.exports = signup;
