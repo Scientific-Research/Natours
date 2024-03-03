0c9116bf332d66b741f3dbdd1e4e7a56dd27af17
Maximilian <scientificresearch309@gmail.com>
2024-03-03 23:17:06 +0100

    we created a function - sendEmail - and with help of a faked email service, we will send our emails to this service and test our emails in development environment

--- utils/email.js
+++ utils/email.js
@@ -3,3 +3,21 @@
 // the name nodemailer says: node + mailer => making the mail using node
 const nodemailer = require('nodemailer');
 
+const sendEmail = (options) => {
+   // 1) Create a transporter
+   const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+         user: process.env.EMAIL_USERNAME,
+         password: process.env.EMAIL_PASSWORD,
+      },
+      // NOTE: an then we have to activate this option in our gamil: "less secure app"
+      // but now, with this service, we can faked to send emails to clients, but this emails
+      // will never reach the clients and instead will trap in my mailtrap.
+      // This is the website: mailtrap.io
+   });
+
+   // 2) Define the email options
+
+   // 3) Actually send the email
+};
