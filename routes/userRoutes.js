778780a5ff1034b7959e8b51817ea5f81a3cf644
Maximilian <scientificresearch309@gmail.com>
2024-03-05 15:43:31 +0100

    I added the route for deleting the user in userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,8 +6,15 @@ const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 
 // using deconstructuring:
-const { getAllUsers, getUser, createUser, updateUser, deleteUser, updateMe } =
-  userController;
+const {
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  updateMe,
+  deleteMe,
+} = userController;
 
 const {
   protect,
@@ -34,9 +41,12 @@ router.patch('/resetPassword/:token', resetPassword);
 router.patch('/updateMyPassword', protect, updatePassword);
 
 // Route for users:
-// NOTE: protect: it means only logged in users can update the user information!
+// NOTE: protect: it means only logged in users can update or delete the user information!
 // NOTE: this route muss stay at the top of other routes which are only intended for users! otherwise, it will not work!
 router.patch('/updateMe', protect, updateMe);
+
+// NOTE: This will not delete the user from database, it will make it unaccessable only!
+router.delete('/deleteMe', protect, deleteMe);
 
 router.route('/').get(getAllUsers).post(createUser);
 // userRouter.route('/').get(getAllUsers).post(createUser);
