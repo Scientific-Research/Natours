2875ae59c4945ee740e21d6f8ec3282719890c73
Maximilian <scientificresearch309@gmail.com>
2024-03-05 12:42:00 +0100

    test for updateMe was done successfully!

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -34,14 +34,16 @@ router.patch('/resetPassword/:token', resetPassword);
 router.patch('/updateMyPassword', protect, updatePassword);
 
 // Route for users:
+// NOTE: protect: it means only logged in users can update the user information!
+// NOTE: this route muss stay at the top of other routes which are only intended for users! otherwise, it will not work!
+router.patch('/updateMe', protect, updateMe);
+
 router.route('/').get(getAllUsers).post(createUser);
 // userRouter.route('/').get(getAllUsers).post(createUser);
 // app.route('/api/v1/users').get(getAllUsers).post(createUser);
+
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 // userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 // app.route('/api/v1/users/:id').get(getUser).patch(updateUser).delete(deleteUser);
-
-// NOTE: protect: it means only logged in users can update the user information!
-router.patch('/updateMe', protect, updateMe);
 
 module.exports = router;
