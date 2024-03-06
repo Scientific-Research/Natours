863d0bec8faa34ff33c1e8652b5cd8f19b20f985
Maximilian <scientificresearch309@gmail.com>
2024-03-06 22:46:49 +0100

    I added protect and restrictTo() to the post route for posting a new Route, only the einlogged users and regular users can post a new Review!

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -3,10 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 const reviewController = require('../controllers/reviewController');
+const authController = require('../controllers/authController');
 
 const { getAllReviews, createReview } = reviewController;
+const { protect, restrictTo } = authController;
 
 router.route('/').get(getAllReviews);
-router.route('/').post(createReview);
+// NOTE: we want that only logged in users and also regular users(not admin or guide users) post a review!
+router.route('/').post(protect, restrictTo('user'), createReview);
 
 module.exports = router;
