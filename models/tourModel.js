10df265661c55f7474d30d47898814863a8ddc1d
Maximilian <scientificresearch309@gmail.com>
2024-03-06 13:22:45 +0100

    I created the startLocation for Geospecial data

--- models/tourModel.js
+++ models/tourModel.js
@@ -3,100 +3,111 @@ const slugify = require('slugify');
 const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
-   {
-      name: {
-         type: String,
-         required: [true, 'A tour must have a name'],
-         unique: true,
-         trim: true,
-         // Adding two validators which are available on Strings!
-         maxLength: [40, 'A tour name must have less or equal than 40 Characters'],
-         minLength: [10, 'A tour name must have more or equal than 10 Characters'],
-         // NOTE: we don't call the isAlpha, rather we use it here!
-         // validate: [validator.isAlpha, 'Tour name must only contains Characters!'],
-         // NOTE: this is not a good validator, because it doesn't accept space between characters!
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+      // Adding two validators which are available on Strings!
+      maxLength: [40, 'A tour name must have less or equal than 40 Characters'],
+      minLength: [10, 'A tour name must have more or equal than 10 Characters'],
+      // NOTE: we don't call the isAlpha, rather we use it here!
+      // validate: [validator.isAlpha, 'Tour name must only contains Characters!'],
+      // NOTE: this is not a good validator, because it doesn't accept space between characters!
+    },
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+      // enum validator is only available for strings
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium or difficult',
       },
-      slug: String,
-      duration: {
-         type: Number,
-         required: [true, 'A tour must have a duration'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      // Adding two validators which are available on Numbers and Dates!
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: {
+      type: Number,
+      // NOTE: we use validate word to create our own validator:
+      // val is here the priceDiscount
+      validate: {
+        validator: function (val) {
+          // NOTE: "this" only points to current doc on NEW document creation!
+          return val < this.price; // 100 < 200 => true, otherwise: false trigger a validation
+          // error
+        },
+        message: 'Discount price ({VALUE}) should be below the regular price!',
+        // NOTE: this VALUE here in a form of Object has the same value as val above!
       },
-      maxGroupSize: {
-         type: Number,
-         required: [true, 'A tour must have a group size'],
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false, // it will not display this item in a tour in Postman, when we get the list
+      // of the tours!
+    },
+    startDates: [Date],
+    // This field is for Query Middleware => find, when tour is public, it would be open,
+    // otherwise: secret
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
+    startLocation: {
+      // NOTE: In order to specify Geospecial data, we need to use GeoJSON.
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
       },
-      difficulty: {
-         type: String,
-         required: [true, 'A tour must have a difficulty'],
-         // enum validator is only available for strings
-         enum: {
-            values: ['easy', 'medium', 'difficult'],
-            message: 'Difficulty is either: easy, medium or difficult',
-         },
-      },
-      ratingsAverage: {
-         type: Number,
-         default: 4.5,
-         // Adding two validators which are available on Numbers and Dates!
-         min: [1, 'Rating must be above 1.0'],
-         max: [5, 'Rating must be below 5.0'],
-      },
-      ratingsQuantity: {
-         type: Number,
-         default: 0,
-      },
-      price: {
-         type: Number,
-         required: [true, 'A tour must have a price'],
-      },
-      priceDiscount: {
-         type: Number,
-         // NOTE: we use validate word to create our own validator:
-         // val is here the priceDiscount
-         validate: {
-            validator: function (val) {
-               // NOTE: "this" only points to current doc on NEW document creation!
-               return val < this.price; // 100 < 200 => true, otherwise: false trigger a validation
-               // error
-            },
-            message: 'Discount price ({VALUE}) should be below the regular price!',
-            // NOTE: this VALUE here in a form of Object has the same value as val above!
-         },
-      },
-      summary: {
-         type: String,
-         trim: true,
-         required: [true, 'A tour must have a description'],
-      },
-      description: {
-         type: String,
-         trim: true,
-      },
-      imageCover: {
-         type: String,
-         required: [true, 'A tour must have a cover image'],
-      },
-      images: [String],
-      createdAt: {
-         type: Date,
-         default: Date.now(),
-         select: false, // it will not display this item in a tour in Postman, when we get the list
-         // of the tours!
-      },
-      startDates: [Date],
-      // This field is for Query Middleware => find, when tour is public, it would be open,
-      // otherwise: secret
-      secretTour: {
-         type: Boolean,
-         default: false,
-      },
-   },
-   {
-      // NOTE: we have to say that we will need the Virtuals, when the data goes to be published at
-      // output!
-      toJSON: { virtuals: true }, // when the data outputted as JSON
-      toObject: { virtuals: true }, // when the data outputted as Object
-   }
+      coordinates: [Number], // this number is coordinates of the point: Langitude and Latitude.
+      address: String,
+      description: String,
+    },
+  },
+  {
+    // NOTE: we have to say that we will need the Virtuals, when the data goes to be published at
+    // output!
+    toJSON: { virtuals: true }, // when the data outputted as JSON
+    toObject: { virtuals: true }, // when the data outputted as Object
+  }
 );
 
 // NOTE: durationWeeks will not persist in DB, it will be there only when get the data and then
@@ -104,10 +115,10 @@ const tourSchema = new mongoose.Schema(
 // defining virtual properties: we have to write regular function instead of arrow function here
 // because arrow function, doesn't take his own "this" key word!
 tourSchema.virtual('durationWeeks').get(function () {
-   return this.duration / 7; // for example, if a tour has 7 days => 7 / 7 => it would be 1 week!
-   // NOTE: "this" points to current document!
-   // NOTE: we can not use the durationWeek as part of a query for example: Tour.find(durationWeek===1)
-   // it will not work because durationWeek is not persistant( doesn't stay) in DB!
+  return this.duration / 7; // for example, if a tour has 7 days => 7 / 7 => it would be 1 week!
+  // NOTE: "this" points to current document!
+  // NOTE: we can not use the durationWeek as part of a query for example: Tour.find(durationWeek===1)
+  // it will not work because durationWeek is not persistant( doesn't stay) in DB!
 });
 
 // NOTE: We define a Document middleware from Mongoose and not from Express and it must be
@@ -116,12 +127,12 @@ tourSchema.virtual('durationWeeks').get(function () {
 // 'save':
 // DOCUMENT MIDDLEWARE: runs before .save() and .create() but not before .insertMany()
 tourSchema.pre('save', function (next) {
-   // NOTE: this function will be called before an actual document is saved to the DB:
-   // console.log(this); // "this" points to the currently proccessed document.
-   // NOTE: to run this middleware, we have to create or save a document in Postman.
-   this.slug = slugify(this.name, { lower: true });
-   next(); // but actually, we don't have the next middleware to call it, anyway, it's good practice
-   // to put it there!
+  // NOTE: this function will be called before an actual document is saved to the DB:
+  // console.log(this); // "this" points to the currently proccessed document.
+  // NOTE: to run this middleware, we have to create or save a document in Postman.
+  this.slug = slugify(this.name, { lower: true });
+  next(); // but actually, we don't have the next middleware to call it, anyway, it's good practice
+  // to put it there!
 });
 
 // tourSchema.pre('save', function (next) {
@@ -140,26 +151,26 @@ tourSchema.pre('save', function (next) {
 // find point out to the current Query Middleware and not current document middleware anymore!
 // tourSchema.pre('find', function (next) {
 tourSchema.pre(/^find/, function (next) {
-   this.find({
-      // NOTE: this doesn't work: secretTour: false, // find the tours in which
-      // secretTour is not equal true.
-      secretTour: { $ne: true }, // find the tours in which secretTour is not equal true.
-      // or it is false, or other tours don't have such field!
-   });
+  this.find({
+    // NOTE: this doesn't work: secretTour: false, // find the tours in which
+    // secretTour is not equal true.
+    secretTour: { $ne: true }, // find the tours in which secretTour is not equal true.
+    // or it is false, or other tours don't have such field!
+  });
 
-   // NOTE: How we can measure the execution time of a query: post() time - pre() time
-   this.start = Date.now(); // Time in milliseconds!
-   next();
+  // NOTE: How we can measure the execution time of a query: post() time - pre() time
+  this.start = Date.now(); // Time in milliseconds!
+  next();
 });
 
 // NOTE: A post middleware for find() => this middleware will run after Query was executed!
 // that's why it has access to the documents which returned!
 tourSchema.post(/^find/, function (docs, next) {
-   // console.log(`Query took ${Date.now() - this.start} milliseconds!`);
-   // Query took 41 milliseconds!
-   // console.log(docs); // it shows us the matched data to the console!
-   // I comment docs out due to not polluting my VSCode Terminal!
-   next();
+  // console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  // Query took 41 milliseconds!
+  // console.log(docs); // it shows us the matched data to the console!
+  // I comment docs out due to not polluting my VSCode Terminal!
+  next();
 });
 
 // NOTE: to get only one tour, we use findById(id) and this is synonyme of findOne when we
@@ -186,17 +197,17 @@ tourSchema.post(/^find/, function (docs, next) {
 // the sectret tour and then aggregation can executes on the tour!
 
 tourSchema.pre('aggregate', function (next) {
-   // NOTE: this is an array and we can put an element in the beginning of an array using
-   // unshift() - shift() put an elemnt at the end of the array.
-   // we put { secretTour: { $ne: true } } at the beginning of the array before it enters the
-   // pipeline => removing all the documents that have secretTour: true
-   // and now, when i run 127.0.0.1:3000/api/v1/tours/tour-stats in postman, i will get 9 tours
-   // which is correct and one is secret that it doesn't come in!
-   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-   console.log(this.pipeline()); // "this" point to the current aggregation Object!
-   // this.pipeline() gives us the three stages that we had before: match, group and sort
-   // it shows us here this as pipeline!
-   next();
+  // NOTE: this is an array and we can put an element in the beginning of an array using
+  // unshift() - shift() put an elemnt at the end of the array.
+  // we put { secretTour: { $ne: true } } at the beginning of the array before it enters the
+  // pipeline => removing all the documents that have secretTour: true
+  // and now, when i run 127.0.0.1:3000/api/v1/tours/tour-stats in postman, i will get 9 tours
+  // which is correct and one is secret that it doesn't come in!
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline()); // "this" point to the current aggregation Object!
+  // this.pipeline() gives us the three stages that we had before: match, group and sort
+  // it shows us here this as pipeline!
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
