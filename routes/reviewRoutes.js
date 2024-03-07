bae3f8a84bc64ea63c20b854e73834744d3061ee
Maximilian <scientificresearch309@gmail.com>
2024-03-07 17:07:11 +0100

    we used the mergeParams as true and tested in Postman, and it works well!

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,6 +1,16 @@
 const express = require('express');
 
-const router = express.Router();
+// const router = express.Router();
+// NOTE: we can here add some options for merge params
+const router = express.Router({ mergeParams: true });
+// NOTE: when we want to have access to the parameters of other routes, it means we are now in reviewRoutes.js and we want to access the tourId but it is not here, rather, it is in tourRouter.js => that's why we use mergeParams: true to give us this possibility to have access to this parameter but in another Route!
+// It means no matter if we get now this route or another route, at the end, it will end in the following route:
+// This route:
+// POST /tour/65342wer/reviews
+// OR this route:
+// POST /reviews
+// at the end it comes with the following route: which is also at the end of this page:
+// router.route('/').post(protect, restrictTo('user'), createReview);
 
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
