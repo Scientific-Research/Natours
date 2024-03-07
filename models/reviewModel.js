e6cae7e91a7c6e798cb16dd1479e4d816fe49439
Maximilian <scientificresearch309@gmail.com>
2024-03-07 12:30:49 +0100

    showing only the user information and not tour info in our reviews!

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -40,15 +40,23 @@ const reviewSchema = mongoose.Schema(
 
 // NOTE: we can also use pre query middleware and don't repeat again the populate code:
 // this middleware populate the name and tour instead of their IDs for Reviews!
-// we see now two IDs for user and tour, but after the population process, It will fill up the user and tour with respected names insread of their IDs.
+// we see now two IDs for user and tour, but after the population process, It will fill up the user and tour with respected names instead of their IDs.
 reviewSchema.pre(/^find/, function (next) {
   // NOTE: we have to call the populate two times, one for tour and another one for user!
+  // this.populate({
+  //   path: 'tour',
+  //   select: 'name',
+  // }).populate({
+  //   path: 'user',
+  //   select: 'name photo', // it must not leak the private infos from user - we just send the name and photos and not something else like email...
+  // });
+
+  // NOTE: we have to call the populate two times, one for tour and another one for user!
+  // NOTE: Actually we don't need to populate the tour info in reviews section for one tour => that's why we omit the first path: 'tour' and therefore, we will have only info about user in reviews section for one tour and not for tour anymore!
   this.populate({
-    path: 'tour',
-    select: 'name',
-  }).populate({
     path: 'user',
-    select: 'name photo', // it must not leak the private infos from user - we just send the name and photos and not something else like email...
+    select: 'name photo ', // it must not leak the private infos from user - we just send the name and photos and not something else like email...
+    // select: '-__v',
   });
   next();
 });
