02a3aafcde256efc3e574ca68be97dd7493d506a
Maximilian <scientificresearch309@gmail.com>
2024-03-12 09:14:26 +0100

    till now, we got the data from Tour and in viewController.js and now, we have to display the tours data in overview.pug

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,20 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+   // 1) Get tour data from collection:
+   const tours = await Tour.find();
+   //  console.log(tours);
+
+   // 2) Build template
+
+   // 3) Render that template using tour data from 1)
+
    res.status(200).render('overview', {
       title: 'All Tours',
+      tours: tours,
    });
-};
+});
 
 exports.getTour = (req, res) => {
    res.status(200).render('tour', {
