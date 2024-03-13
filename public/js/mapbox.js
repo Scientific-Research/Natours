a22e8c34b4a68c34eb5cdff6604df34f66e4e761
Maximilian <scientificresearch309@gmail.com>
2024-03-13 10:15:43 +0100

    finally, i got the locations for selected tour in inspect!

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,1 +1,4 @@
-console.log('Hello from the client side :D');
+/* eslint-disable */
+
+const locations = JSON.parse(document.getElementById('map').dataset.locations); // we get the data for location and we have to convert it to the JSON object.
+console.log(locations);
