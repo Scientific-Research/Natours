f3a14177507ac8fc7026a1f2cabc34049afd3fb3
Maximilian <scientificresearch309@gmail.com>
2024-03-13 21:33:02 +0100

    the first step to create the login form=> create a route for that in viewRoutes.js

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const viewsController = require('../controllers/viewsController');
 
-const { getOverview, getTour } = viewsController;
+const { getOverview, getTour, getLoginForm } = viewsController;
 
 // NOTE: This route is for pug engine template:
 router.get('/', getOverview);
 router.get('/tour/:slug', getTour);
+router.get('/login', getLoginForm);
 
+//   /login
 module.exports = router;
