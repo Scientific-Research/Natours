5abbd3de81b570f748a06f83aa008300a95a5018
Maximilian <scientificresearch309@gmail.com>
2024-03-14 11:32:34 +0100

    protect our Tour in a way that, when i click the details, it doesn't go in, unless i bin already einlogged!

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const viewsController = require('../controllers/viewsController');
+const authConteroller = require('../controllers/authController');
 
 const { getOverview, getTour, getLoginForm } = viewsController;
+const { protect } = authConteroller;
 
 // NOTE: This route is for pug engine template:
 router.get('/', getOverview);
-router.get('/tour/:slug', getTour);
+// NOTE: protect our Tour in a way that, when i click the details, it doesn't go in, unless i am already einlogged!
+router.get('/tour/:slug', protect, getTour);
 router.get('/login', getLoginForm);
 
 //   /login
