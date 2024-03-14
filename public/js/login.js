9f389b49676d786b6e2af60196b37a5106c16c37
Maximilian <scientificresearch309@gmail.com>
2024-03-14 15:15:27 +0100

    alert the error and success messages and set the timeout ti reload the page!

--- public/js/login.js
+++ public/js/login.js
@@ -1,14 +1,22 @@
 const login = async (email, password) => {
-   console.log(email, password);
+   //  console.log(email, password);
    //  alert(email, password);
    // NOTE: to get the http request for the login, we use the axios from CDN to fecth the data!
 
    // Usage:
    try {
       const res = await axios.post('http://127.0.0.1:3000/api/v1/users/login', { email, password });
-      console.log('response from axios:', res.data);
+
+      // console.log('response from axios:', res.data); // data is our JSON data!
+      if (res.data.status === 'success') {
+         alert('Logged in successfully!');
+         window.setTimeout(() => {
+            location.assign('/');
+         }, 1500);
+      }
    } catch (error) {
-      console.log('Error:', error.response.data);
+      // console.log('Error:', error.response.data);
+      alert(error.response.data.message); // we get the message from our data => JSON data!
    }
 };
 
