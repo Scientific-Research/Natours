076e3973627a58757ffef2c650c308ad3d5dc6c3
Maximilian <scientificresearch309@gmail.com>
2024-03-15 23:59:26 +0100

    updating user data using API works well!

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,11 @@
 import '@babel/polyfill';
 import { login, logout } from './login';
+import { updateData } from './updateSettings';
 
 // DOM ELEMENTS
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
 
 // DELEGATION
 
@@ -20,3 +22,11 @@ if (loginForm)
 
 // NOTE: when somebody click on the logout button, the logout function will be executed!
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if (userDataForm)
+   userDataForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const name = document.getElementById('name').value;
+      const email = document.getElementById('email').value;
+      updateData(name, email);
+   });
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,1 +1,16 @@
 // updateData
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+export const updateData = async (name, email) => {
+   try {
+      const res = await axios.patch('http://127.0.0.1:3000/api/v1/users/updateMe', { name, email });
+
+      // console.log('response from axios:', res.data); // data is our JSON data!
+      if (res.data.status === 'success') {
+         showAlert('success', 'Data updated successfully!');
+      }
+   } catch (error) {
+      showAlert('error', error.response.data.message);
+   }
+};
