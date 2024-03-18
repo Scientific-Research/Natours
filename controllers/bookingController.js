90d6aa7db68b9da549aa738c9b65df49563960ac
Maximilian <scientificresearch309@gmail.com>
2024-03-18 14:07:51 +0100

    the second part for creating the checkout session finished now!

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,3 +1,4 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
@@ -10,6 +11,27 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
    const tour = await Tour.findById(tourId); // NOTE: findById needs only an id and not two Ids with a equal sign in between!
 
    // 2) Create checkout session
+   const session = await stripe.checkout.session.create({
+      // This part is the information about session itself
+      payment_method_types: ['card'],
+      success_url: `${req.protocol}://${req.get('host')}/`,
+      cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+      customer_email: req.user.email, // user is already at request, because the route is protected!
+      client_reference_id: tourId,
+      // This is the information about session itself
+
+      // This part is information about product that user is going to purchase!
+      line_items: [
+         {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+            amount: tour.price * 100,
+            currency: 'usd',
+            quantity: 1,
+         },
+      ],
+   });
 
    // 3) Create session as response and send it to the client
 });
