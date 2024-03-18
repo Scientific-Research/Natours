d86b145c4a48d84177455b3baada13bd0f0ad4ec
Maximilian <scientificresearch309@gmail.com>
2024-03-18 12:58:21 +0100

    using findById to get the tourId

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -3,6 +3,13 @@ const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 const AppError = require('../utils/appError');
 
-exports.getCheckoutSession = (req, res, next) => {
-   const tourID = req.params.tourID;
-};
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+   // 1) Get the currently booked tour
+   const tourId = req.params.tourId;
+   //  const tour = await Tour.find((tour) => tourId === req.tour.id);
+   const tour = await Tour.findById(tourId); // NOTE: findById needs only an id and not two Ids with a equal sign in between!
+
+   // 2) Create checkout session
+
+   // 3) Create session as response and send it to the client
+});
