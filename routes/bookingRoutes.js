1bbf1be7bb7cd6a58332418ecdb2f7cb7dc4c407
Maximilian <scientificresearch309@gmail.com>
2024-03-19 15:06:15 +0100

    all routes for CRUD operations done in bookingRoutes.js

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,9 +4,29 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-const { protect } = authController;
-const { getCheckoutSession } = bookingController;
+const { protect, restrictTo } = authController;
+router.use(protect); // all the following routes use the protect middleware, therefore, we don't need to apply protect middleware one by one, rather only in one place and for all routes!
 
-router.get('/checkout-session/:tourId', protect, getCheckoutSession);
+const {
+   getCheckoutSession,
+   getAllBooking,
+   getBooking,
+   createBooking,
+   updateBooking,
+   deleteBooking,
+} = bookingController;
+
+router.get('/checkout-session/:tourId', getCheckoutSession);
+
+// CRUD operations for booking section:
+
+router.use(restrictTo('admin', 'lead-guide')); // all following routes are allowed to be executed only by admin or lead-guide and nobody else!
+
+router.get('/', getAllBooking);
+router.post('/', createBooking);
+
+router.get('/:id', getBooking);
+router.patch('/:id', updateBooking);
+router.delete('/:id', deleteBooking);
 
 module.exports = router;
